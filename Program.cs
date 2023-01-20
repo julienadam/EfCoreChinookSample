9abb2bd782e49ee628af5f9c8ac6c9c7cf79a0af
Julien Adam <julien.adam@arcodia.fr>
2023-01-20 14:14:59 +0100

    Initial berfore import

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ChinookExercice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Chinook;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
